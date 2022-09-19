df0c5df37ae137847eaff97d98b3bbe4f4cfc9fa
kobby-wilf <111072744+kobby-wilf@users.noreply.github.com>
2022-09-18 23:23:01 -0700

    Add files via upload

---
+++ details.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shipping</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+
+
+
+</head>
+<body>
+
+
+
+<header>
+      
+      <img src="shipimgs/logo12.png" alt="">
+
+      <nav class="narbar">
+  
+          <ul>
+
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="#">SERVICES</a></li>
+              <li><a href="#">SHIPPING</a></li>
+              <li><a href="#">TRACKING</a></li>
+              <li><a href="#">BLOG</a></li>
+              <li><a href="#">PAGES</a></li>
+              <li><a href="#footer">CONTACT US</a></li>
+  
+          </ul>
+          
+      </nav>
+      <!-- <form action="" class="search-form">
+          <input type="search" name="" placeholder="search here..." id="search-box">
+          <label for="search-box" id="glass" class="fas fa-search"></label>
+      </form> -->
+      <div id="menu-bar" class="fas fa-bars"></div>
+      
+
+  </header>
+
+
+  <table class="table table-success table-striped">
+
+
+
+
+
+</body>
+</html>
